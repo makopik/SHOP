1d39500b35d756e2f00b9c86c368bb620ff38253
Максим Красовский <mkrasovskii@cinimex.ru>
2025-06-09 09:57:10 +0300

    add api endpoints

---
+++ src/core/services/api.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const BASE_URL = "https://fakestoreapi.com";
+
+export const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  prepareHeaders: (headers) => {
+    return headers;
+  },
+});
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery,
+  endpoints: () => ({}),
+});
